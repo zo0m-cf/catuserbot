fdaabc844c1d8c12c94ce61f3e744153624e38ca
XMTS.CF <60861876+xmtscf@users.noreply.github.com>
2020-10-23 11:58:56 -0300

    Update help, inlinebot

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -40,7 +40,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             buttons = paginate_help(0, CMD_LIST, "helpme")
             result = builder.article(
                 "© Userbot Help",
-                text="{}\nCurrently Loaded Plugins: {}".format(query, len(CMD_LIST)),
+                text="{}\nPlugins carregados atualmente: {}".format(query, len(CMD_LIST)),
                 buttons=buttons,
                 link_preview=False,
             )
@@ -145,7 +145,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             # https://t.me/TelethonChat/115200
             await event.edit(buttons=buttons)
         else:
-            reply_pop_up_alert = "Please get your own catuserbot, and don't use mine! Join @catuserbot17 help"
+            reply_pop_up_alert = "Por favor, adquira seu próprio catuserbot e não use o meu!"
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
     @tgbot.on(
@@ -162,7 +162,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             # https://t.me/TelethonChat/115200
             await event.edit(buttons=buttons)
         else:
-            reply_pop_up_alert = "Please get your own catuserbot, and don't use mine! Join @catuserbot17 help "
+            reply_pop_up_alert = "Por favor, adquira seu próprio catuserbot e não use o meu! "
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
     @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"secret_(.*)")))
@@ -178,11 +178,11 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
                     encrypted_tcxt = message["text"]
                     reply_pop_up_alert = encrypted_tcxt
                 else:
-                    reply_pop_up_alert = "why were you looking at this shit go away and do your own work, idiot"
+                    reply_pop_up_alert = "por que você estava olhando essa merda vai embora e faça seu próprio trabalho idiota"
             except KeyError:
-                reply_pop_up_alert = "This message no longer exists in bot server"
+                reply_pop_up_alert = "Esta mensagem não existe mais no servidor bot"
         else:
-            reply_pop_up_alert = "This message no longer exists "
+            reply_pop_up_alert = "Esta mensagem não existe mais "
         await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
     @tgbot.on(
@@ -221,7 +221,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
                         caption=plugin_name,
                     )
         else:
-            reply_pop_up_alert = "Please get your own catuserbot, and don't use mine! Join @catuserbot17 help "
+            reply_pop_up_alert = "Por favor, adquira seu próprio catuserbot e não use o meu! "
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
     @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"close")))
@@ -229,7 +229,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
         if event.query.user_id == bot.uid:
             await event.edit("menu closed")
         else:
-            reply_pop_up_alert = "Please get your own catuserbot, and don't use mine! Join @catuserbot17 help "
+            reply_pop_up_alert = "Por favor, adquira seu próprio catuserbot e não use o meu! "
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
     @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"stats")))
--- userbot/plugins/help.py
+++ userbot/plugins/help.py
@@ -20,13 +20,13 @@ async def cmd_list(event):
     input_str = event.pattern_match.group(1)
     if input_str == "text":
         string = (
-            "Total {count} commands found in {plugincount} plugins of catuserbot\n\n"
+            "Total {count} comandos encontrados em {plugincount} plugins de catuserbot\n\n"
         )
         catcount = 0
         plugincount = 0
         for i in sorted(CMD_LIST):
             plugincount += 1
-            string += f"{plugincount}) Commands found in Plugin " + i + " are \n"
+            string += f"{plugincount}) Comandos encontrados no Plugin " + i + " sao \n"
             for iter_list in CMD_LIST[i]:
                 string += "    " + str(iter_list)
                 string += "\n"
@@ -43,14 +43,14 @@ async def cmd_list(event):
                 .get("key")
             )
             url = f"https://nekobin.com/{key}"
-            reply_text = f"**All commands of the catuserbot can be seen [here]({url})**"
+            reply_text = f"**Todos os comandos do catuserbot podem ser vistos [here]({url})**"
             await event.edit(reply_text)
             return
         await event.edit(string.format(count=catcount, plugincount=plugincount))
         return
     if input_str:
         if input_str in CMD_LIST:
-            string = "<b>{count} Commands found in plugin {input_str}:</b>\n\n"
+            string = "<b>{count} Comandos encontrados no plugin {input_str}:</b>\n\n"
             catcount = 0
             for i in CMD_LIST[input_str]:
                 string += f"  ◆  <code>{i}</code>"
@@ -60,15 +60,15 @@ async def cmd_list(event):
                 string.format(count=catcount, input_str=input_str), parse_mode="HTML"
             )
         else:
-            await event.edit(input_str + " is not a valid plugin!")
+            await event.edit(input_str + " não é um plugin válido!")
             await asyncio.sleep(3)
             await event.delete()
     else:
         if HELPTYPE is True:
-            help_string = f"Userbot Helper.. Provided by {DEFAULTUSER}\
-                          \nUserbot Helper to reveal all the plugin names\
-                          \n__Do__ `.help` __plugin_name for commands, in case popup doesn't appear.__\
-                          \nDo `.info` plugin_name for usage"
+            help_string = f"Userbot Helper .. Fornecido por {DEFAULTUSER}\
+                          \nUserbot Helper para revelar todos os nomes de plug-ins\
+                          \n__Envie__ `.help` __plugin_name para comandos, caso o pop-up não apareça.__\
+                          \nOu `.info` plugin_name para uso"
             tgbotusername = Var.TG_BOT_USER_NAME_BF_HER
             results = await bot.inline_query(  # pylint:disable=E0602
                 tgbotusername, help_string
@@ -76,9 +76,9 @@ async def cmd_list(event):
             await results[0].click(event.chat_id, reply_to=reply_to_id, hide_via=True)
             await event.delete()
         else:
-            string = "<b>Please specify which plugin do you want help for !!\
-                \nNumber of plugins : </b><code>{count}</code>\
-                \n<b>Uso:</b> <code>.help</code> plugin name\n\n"
+            string = "<b>Por favor, especifique para qual plugin você quer ajuda !!\
+                \nNúmero de plugins : </b><code>{count}</code>\
+                \n<b>Uso:</b> <code>.help</code> nome plugin\n\n"
             catcount = 0
             for i in sorted(CMD_LIST):
                 string += "◆" + f"<code>{str(i)}</code>"
@@ -91,12 +91,12 @@ async def cmd_list(event):
 async def info(event):
     input_str = event.pattern_match.group(1)
     if input_str == "text":
-        string = "Total {count} commands found in {plugincount} sudo plugins of catuserbot\n\n"
+        string = "Total {count} comandos encontrados em {plugincount} sudo plugins of catuserbot\n\n"
         catcount = 0
         plugincount = 0
         for i in sorted(SUDO_LIST):
             plugincount += 1
-            string += f"{plugincount}) Commands found in Plugin " + i + " are \n"
+            string += f"{plugincount}) Comandos encontrados no Plugin " + i + " are \n"
             for iter_list in SUDO_LIST[i]:
                 string += "    " + str(iter_list)
                 string += "\n"
@@ -113,14 +113,14 @@ async def info(event):
                 .get("key")
             )
             url = f"https://nekobin.com/{key}"
-            reply_text = f"All commands of the catuserbot are [here]({url})"
+            reply_text = f"Todos os comandos do catuserbot são [here]({url})"
             await event.reply(reply_text)
             return
         await event.reply(string.format(count=catcount, plugincount=plugincount))
         return
     if input_str:
         if input_str in SUDO_LIST:
-            string = "<b>{count} Commands found in plugin {input_str}:</b>\n\n"
+            string = "<b>{count} Comandos encontrados no plugin {input_str}:</b>\n\n"
             catcount = 0
             for i in SUDO_LIST[input_str]:
                 string += f"  ◆  <code>{i}</code>"
@@ -130,13 +130,13 @@ async def info(event):
                 string.format(count=catcount, input_str=input_str), parse_mode="HTML"
             )
         else:
-            reply = await event.reply(input_str + " is not a valid plugin!")
+            reply = await event.reply(input_str + " não é um plugin válido!")
             await asyncio.sleep(3)
             await event.delete()
             await reply.delete()
     else:
-        string = "<b>Please specify which plugin do you want help for !!\
-            \nNumber of plugins : </b><code>{count}</code>\
+        string = "<b>Por favor, especifique para qual plugin você quer ajuda !!\
+            \nNúmero de plugins : </b><code>{count}</code>\
             \n<b>Uso:</b> <code>.help</code> plugin name\n\n"
         catcount = 0
         for i in sorted(SUDO_LIST):
@@ -155,12 +155,12 @@ async def info(event):
         if args in CMD_HELP:
             await edit_or_reply(event, str(CMD_HELP[args]))
         else:
-            event = await edit_or_reply(event, "Please specify a valid plugin name.")
+            event = await edit_or_reply(event, "Especifique um nome de plugin válido.")
             await asyncio.sleep(3)
             await event.delete()
     else:
-        string = "<b>Please specify which plugin do you want help for !!\
-            \nNumber of plugins : </b><code>{count}</code>\
+        string = "<b>Por favor, especifique para qual plugin você quer ajuda !!\
+            \nNúmero de plugins : </b><code>{count}</code>\
             \n<b>Uso : </b><code>.info</code> <plugin name>\n\n"
         catcount = 0
         for i in sorted(CMD_HELP):
@@ -179,7 +179,7 @@ async def _(event):
     result = await bot(functions.help.GetNearestDcRequest())
     result = (
         yaml_format(result)
-        + "\n\n**List Of Telegram Data Centres:**\
+        + "\n\n**Lista de data centers do Telegram:**\
                 \nDC1 : Miami FL, USA\
                 \nDC2 : Amsterdam, NL\
                 \nDC3 : Miami FL, USA\
