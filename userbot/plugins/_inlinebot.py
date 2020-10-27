ec4aa3db44b7caa87e30d79a7da52b9068290642
XMTS.CF <60861876+xmtscf@users.noreply.github.com>
2020-10-27 15:09:32 -0300

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -228,7 +228,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
     @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"close")))
     async def on_plug_in_callback_query_handler(event):
         if event.query.user_id == bot.uid:
-            await event.edit("menu closed")
+            await event.edit("menu fechado")
         else:
             reply_pop_up_alert = "Por favor, adquira seu próprio catuserbot e não use o meu! "
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
@@ -280,7 +280,7 @@ def paginate_help(page_number, loaded_plugins, prefix):
                 custom.Button.inline(
                     "⌫", data="{}_prev({})".format(prefix, modulo_page)
                 ),
-                custom.Button.inline("Close", data="close"),
+                custom.Button.inline("Fechar", data="close"),
                 custom.Button.inline(
                     "⌦", data="{}_next({})".format(prefix, modulo_page)
                 ),
